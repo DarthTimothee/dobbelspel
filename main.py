3b4a99af0986508a38f04f28f050953d935f177a
DarthTimothee <timoverlaan@gmail.com>
2020-12-14 00:18:29 +0100

    fix missing game rules 2

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def simulate(n, prev_score=0, strategy=strat.choose_all):
 
     remaining = len(remove_three) - remove_three.count(1) - remove_three.count(5)
     score = prev_score + max_score(dice)
-    if score < 350:
-        score = 0
 
     if remaining == 0:
         # print(f"no choice, score = {score}")
         return score + simulate(6, strategy=strategy)
     if remaining == 1:
         # print(f"no choice, score = {score}")
+        if score < 350:
+            score = 0
         return score
     if remaining == n:
         # print(f"no luck, score = 0")
