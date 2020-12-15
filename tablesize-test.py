a49e6787f3961119928f9340283494a7d1c51e63
DarthTimothee <timoverlaan@gmail.com>
2020-12-15 16:22:18 +0100

    Q-table with defaultdict

--- tablesize-test.py
+++ tablesize-test.py
@@ -1,38 +1,27 @@
+from collections import defaultdict
+from pprint import pprint
+
 if __name__ == "__main__":
 
     dice = [1, 2, 3, 4, 5, 6]
-    D = []
+    D = defaultdict(lambda: 0)
 
     for a in dice:
         for b in dice:
-            l = [a, b]
-            l.sort()
-            if l not in D:
-                D.append(l)
+            D[tuple(sorted([a, b]))] += 1
 
             for c in dice:
-                l = [a, b, c]
-                l.sort()
-                if l not in D:
-                    D.append(l)
+                D[tuple(sorted([a, b, c]))] += 1
 
                 for d in dice:
-                    l = [a, b, c, d]
-                    l.sort()
-                    if l not in D:
-                        D.append(l)
+                    D[tuple(sorted([a, b, c, d]))] += 1
 
                     for e in dice:
-                        l = [a, b, c, d, e]
-                        l.sort()
-                        if l not in D:
-                            D.append(l)
+                        D[tuple(sorted([a, b, c, d, e]))] += 1
 
                         for f in dice:
-                            l = [a, b, c, d, e, f]
-                            l.sort()
-                            if l not in D:
-                                D.append(l)
+                            D[tuple(sorted([a, b, c, d, e, f]))] += 1
 
+    pprint(dict(D))
     print(len(D))
     # Out[1]: 917
